ea75b3920ea0ae57f0f35f76f52a1375504d8112
Charithavya <75021368+Charithavya@users.noreply.github.com>
2024-04-12 20:32:33 -0500

    script.js

--- script.js
+++ script.js
@@ -14,33 +14,22 @@ loginBtn.addEventListener('click', () => {
 
 
 function auth() {
-    if (validateForm()) {
+   
         var first = document.getElementById("first").value;
-        var password = document.getElementById("password").value;
+        
 
-        if (first == "Sara" && password == "sara@ara") {
+        if (first == "Sara" ) {
             window.location.assign("page1.html");
-        } else if (first == "Gregg" && password == "gregg@egg") {
+        } else if (first == "Gregg" ) {
             window.location.assign("page2.html");
         } else {
             alert("Invalid information");
         }
-    }
+
 }
 
-function validateForm() {
-    const password = document.getElementById("password").value;
 
-    // Validate password
-    if (password === "" || (password !== "sara@ara" && password !== "gregg@egg")) {
-        document.getElementById("passwordError").style.display = "inline";
-        return false;
-    } else {
-        document.getElementById("passwordError").style.display = "none";
-    }
 
-    return true; // Form is valid, proceed with authentication
-}
 
 
         
